a78b463aa05f874edbee0d2c9e9cbd2af6fc5c16
honeypolarbear <cjkim6375@naver.com>
2022-02-08 10:49:59 +0900

    20220208
    124

--- Dodge/Assets/scripts/GameManager.cs
+++ Dodge/Assets/scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//ui 관련 라이브러리
+using UnityEngine.SceneManagement;// 씬 관리 관련 라이브러리
 
-public class Rotator : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public float rotationSpeed = 60f;
     // Start is called before the first frame update
-    
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0f, rotationSpeed*Time.deltaTime, 0f);
+        
     }
 }
