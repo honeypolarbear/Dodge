a57178f1e5dc3fe464e44ec67c871ea70e330cd7
honeypolarbear <cjkim6375@naver.com>
2022-02-09 12:45:58 +0900

    20220209
    777

--- Dodge/Assets/scripts/GameManager.cs
+++ Dodge/Assets/scripts/GameManager.cs
@@ -6,15 +6,33 @@ using UnityEngine.SceneManagement;// 씬 관리 관련 라이브러리
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
     // Start is called before the first frame update
     void Start()
     {
-        
+        surviveTime = 0;
+        isGameover = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time:" + (int)surviveTime;
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.R)
+                {
+                SceneManager.LoadScene("SampleScene");
+                    }
+        }
     }
 }
